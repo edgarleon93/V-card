f6a6777741f5e7747986d1e54bd02b8f6f63bf69
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-02-07 20:56:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return ( 
+<div class="grid py-10 place-items-center h-screen w-screen ">
+
+  <div className="container grid grid-cols-12	h-full">
+
+    <div className=" col-span-10 sm:col-span-11">
+      <h1 className=" lg:text-9xl lg:leading-[6.5rem] md:text-8xl md:leading-[4.5rem] text-6xl leading-[3rem] font-medium text-start overscroll-contain">CREATIVE <br />DEVELOPER</h1>
+    </div>
+    <div className=" col-span-2 sm:col-span-1">
+      <a className=" lg:text-2xl md:text-2xl font-medium text-end hover:underline items-center">About</a>
+    </div>
+
+    <div className="col-span-12 sm:col-span-12">
+      <div class="photo mt-8 mb-8 bg-no-repeat bg-contain	md:w-1/2 "> <img src="/src/image/photo.png" alt="" /> </div>
+    </div>
+
+    <div className=" col-span-2 place-items-start h-50 pt-8 ">
+        <img className='' src="/src/image/arrow.svg " alt="" />
+    </div>
+    <div className=" col-span-10 h-50">
+        <div className="col-span-7 h-50"></div>
+        <h1 className="col-span-10 lg:text-9xl lg:leading-[6.5rem] md:text-8xl md:leading-[4.5rem] text-6xl leading-[3rem] font-medium text-end ">PRINGUET <br /> EDGAR</h1>
+
+    </div>
+    {/* <div className=" col-span-7 h-50"> */}
+        {/* <h1 className="col-span-10 lg:text-9xl lg:leading-[6.5rem] md:text-8xl md:leading-[4.5rem] text-6xl leading-[3rem] font-medium text-end ">PRINGUET <br /> EDGAR</h1> */}
+    {/* </div> */}
+    
+  </div>
+  
+</div>   
+  )
+}
+
+export default App
